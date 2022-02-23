182cb90f0a935f34e189883fd08af5a013ffcefb
HA <>
2022-02-23 12:42:24 +0800

    save user info to db

--- src/api/guilds.ts
+++ src/api/guilds.ts
@@ -4,6 +4,8 @@ import { client } from "../main.js";
 import { generateFlashsignerAddress } from '@nervina-labs/flashsigner'
 import NodeRsa from 'node-rsa';
 import { Buffer } from 'buffer';
+import db from "../database";
+import { User } from "../shared/firestoreTypes";
 
 @Router()
 export class API {
@@ -28,34 +30,51 @@ export class API {
   }
 
   @Get('/sign-success')
-  verifySig(context: Context) {
-    context.body = context.request;
+  async verifySig(context: Context) {
 
     const { flashsigner_data } = context.request.query;
     const data = JSON.parse(flashsigner_data as string);
     console.log(data)
-    const { lock, message, sig: signature } = data.result;
+    const { message, sig: signature } = data.result;
     const response = {
       message,
       "signature": signature.slice(520),
       "pubkey": signature.slice(0, 520)
     }
-    console.log('response: ', response)
-
     const key = new NodeRsa()
     const buf = Buffer.from(response.pubkey, 'hex')
     const e = buf.slice(0, 4).reverse()
     const n = buf.slice(4).reverse()
     key.importKey({ e, n }, 'components-public')
     key.setOptions({ signingScheme: 'pkcs1-sha256' })
-    const result = key.verify(
+    const isSigValid = key.verify(
       Buffer.from(response.message),
       Buffer.from(response.signature, 'hex')
     )
 
-    console.log(result)
+    context.body = `Signature verified result: ${isSigValid}`;
+
+    if (!isSigValid) return;
 
     const address = generateFlashsignerAddress(response.pubkey)
     console.log('address: ', address)
+
+
+    const user: User = {
+      wallet: address,
+    };
+
+    const userDoc = await db
+      .collection("users")
+      .doc(address)
+      .get();
+
+    console.log('userDoc: ', userDoc)
+    if (userDoc.exists) return;
+    await db
+      .collection("users")
+      .doc(address)
+      .set(user);
+
   }
 }
