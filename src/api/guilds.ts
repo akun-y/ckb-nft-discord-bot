2ac33806f157d28a8698e434f92816923d2de8e5
Billie Zhang <billiecoder@gmail.com>
2022-03-04 21:24:03 +0800

    format code

--- src/api/guilds.ts
+++ src/api/guilds.ts
@@ -1,22 +1,26 @@
-import { Get, Router } from "@discordx/koa";
-import type { Context } from "koa";
-import { client } from "../main.js";
+import { Get, Router } from '@discordx/koa'
+import type { Context } from 'koa'
+import { client } from '../main.js'
 import { generateFlashsignerAddress } from '@nervina-labs/flashsigner'
-import NodeRsa from 'node-rsa';
-import { Buffer } from 'buffer';
-import db from "../database";
-import { User } from "../shared/firestoreTypes";
-import * as dotenv from "dotenv";
-import jwt from "jsonwebtoken";
+import NodeRsa from 'node-rsa'
+import { Buffer } from 'buffer'
+import db from '../database'
+import { User } from '../shared/firestoreTypes'
+import * as dotenv from 'dotenv'
+import jwt from 'jsonwebtoken'
 
-dotenv.config();
+dotenv.config()
 
-const DISCORD_VERIFICATION_SECRET = process.env.DISCORD_VERIFICATION_SECRET || "secret";
-console.log("DISCORD_VERIFICATION_SECRET: ", process.env.DISCORD_VERIFICATION_SECRET)
+const DISCORD_VERIFICATION_SECRET =
+  process.env.DISCORD_VERIFICATION_SECRET || 'secret'
+console.log(
+  'DISCORD_VERIFICATION_SECRET: ',
+  process.env.DISCORD_VERIFICATION_SECRET
+)
 
 @Router()
 export class API {
-  @Get("/")
+  @Get('/')
   index(context: Context) {
     context.body = `
       <div style="text-align: center">
@@ -28,25 +32,24 @@ export class API {
           <a href="https://www.npmjs.com/package/@discordx/koa">@discordx/koa</a>
         </p>
       </div>
-    `;
+    `
   }
 
   @Get()
   guilds(context: Context) {
-    context.body = `${client.guilds.cache.map((g) => `${g.id}: ${g.name}\n`)}`;
+    context.body = `${client.guilds.cache.map((g) => `${g.id}: ${g.name}\n`)}`
   }
 
   @Get('/sign-success')
   async verifySig(context: Context) {
-
-    const { flashsigner_data } = context.request.query;
-    const data = JSON.parse(flashsigner_data as string);
+    const { flashsigner_data } = context.request.query
+    const data = JSON.parse(flashsigner_data as string)
     console.log(data)
-    const { message, sig: signature } = data.result;
+    const { message, sig: signature } = data.result
     const response = {
       message,
-      "signature": signature.slice(520),
-      "pubkey": signature.slice(0, 520)
+      signature: signature.slice(520),
+      pubkey: signature.slice(0, 520)
     }
     const key = new NodeRsa()
     const buf = Buffer.from(response.pubkey, 'hex')
@@ -59,59 +62,56 @@ export class API {
       Buffer.from(response.signature, 'hex')
     )
 
-    context.body = `Signature verified result: ${isSigValid}`;
+    context.body = `Signature verified result: ${isSigValid}`
 
-    if (!isSigValid) return;
+    if (!isSigValid) return
 
     const address = generateFlashsignerAddress(response.pubkey)
     console.log('address: ', address)
 
-
-    const decoded = jwt.verify(message, DISCORD_VERIFICATION_SECRET) as jwt.JwtPayload
+    const decoded = jwt.verify(
+      message,
+      DISCORD_VERIFICATION_SECRET
+    ) as jwt.JwtPayload
     console.log('decoded: ', decoded)
     const { userId, guildId } = decoded
 
     const user: User = {
       wallet: address,
       userId,
-      guildId,
-    };
+      guildId
+    }
 
     const docKey = `${guildId}-${userId}`
 
-    const userDoc = await db
-      .collection("users")
-      .doc(docKey)
-      .get();
+    const userDoc = await db.collection('users').doc(docKey).get()
 
     console.log('exists: ', userDoc.exists)
 
     if (userDoc.exists && userDoc.data()!.wallet === address) {
       console.log('User info already exists')
-      return;
+      return
     }
 
     try {
-      await db
-        .collection("users")
-        .doc(docKey)
-        .set(user);
+      await db.collection('users').doc(docKey).set(user)
     } catch (error) {
       console.error('Error happened for saving user info: ', error)
     }
 
-    const guild = await client.guilds.fetch(guildId);
-    const member = await guild.members.fetch(userId);
+    const guild = await client.guilds.fetch(guildId)
+    const member = await guild.members.fetch(userId)
 
     // TODO: Add role to user
+    // check if user's address has nfts
     const botManagerRole = guild.roles.cache.find(
       (role) => role.name == 'Rostra guild contributor'
-    )!;
+    )!
 
     console.log('botManagerRole: ', botManagerRole)
 
     try {
-      member.roles.add(botManagerRole);
+      member.roles.add(botManagerRole)
       console.log('role added: ', botManagerRole.name)
     } catch (error) {
       console.error('Error happened for adding role: ', error)
--- src/utils/setupPermissions.ts
+++ src/utils/setupPermissions.ts
@@ -1,44 +1,44 @@
-import { Guild } from "discord.js";
-import * as dotenv from "dotenv";
+import { Guild } from 'discord.js'
+import * as dotenv from 'dotenv'
 
-dotenv.config();
+dotenv.config()
 
 console.log('BOT_MANAGER_ROLE: ', process.env.BOT_MANAGER_ROLE)
 export default async (guild: Guild) => {
-  // Check if an existing lunar commander role exists in the server
+  // Check if an existing Rostra commander role exists in the server
   const existingManagerRole = guild.roles.cache.find(
     (role) => role.name === process.env.BOT_MANAGER_ROLE
-  );
+  )
 
   // If no such role exists, create it
   const managerRole = existingManagerRole
     ? existingManagerRole
     : await guild.roles.create({
         name: process.env.BOT_MANAGER_ROLE,
-        color: "BLUE",
-        reason: "Managing the guild assistant.",
-      });
+        color: 'BLUE',
+        reason: 'Managing the guild assistant.'
+      })
 
   // now update the permissions to allow for BOT_MANAGER_ROLE to use specific commands
   const permissions = [
     {
       id: managerRole.id,
-      type: "ROLE" as const,
-      permission: true,
-    },
-  ];
+      type: 'ROLE' as const,
+      permission: true
+    }
+  ]
 
-  const registeredCommands = await guild.commands.fetch();
+  const registeredCommands = await guild.commands.fetch()
 
   // get the configured command
   const configureCommand = registeredCommands.find((command) => {
-    return command.name == "guild-setup";
-  });
+    return command.name == 'guild-setup'
+  })
 
   if (!configureCommand) {
-    throw new Error("Could not find guild-setup command!");
+    throw new Error('Could not find guild-setup command!')
   }
 
   // add command permission
-  await configureCommand!.permissions.add({ permissions });
-};
+  await configureCommand!.permissions.add({ permissions })
+}
